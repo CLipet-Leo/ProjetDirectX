9ed289c75e86d4efdf688683686edb9bc3b26e70
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2024-02-22 18:25:40 +0100

    Initial commit

---
+++ MoteurDirectX/IncludeLib.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Windows.h>
+#include <wrl.h>
+
+#include <dxgi.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include "../DirectX/d3dx12.h"
---
+++ MoteurDirectX/Renderer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "IncludeLib.h"
+using Microsoft::WRL::ComPtr; 
+
+class Renderer
+{
+public:
+	
+
+	Renderer();
+
+};
+
