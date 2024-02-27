598c55697ad8583885ac537e57faf4c95f6c9d9e
Zolteks <96941840+Zolteks@users.noreply.github.com>
2024-02-27 11:19:27 +0100

    structures definition

---
+++ MoteurDirectX/Shader.cpp
@@ -0,0 +1,41 @@
+#include "Shader.h"
+#include "IncludeLib.h"
+#include <DirectXPackedVector.h>
+#include <vector>
+
+using namespace DirectX;
+
+struct VertexColor
+{
+    XMFLOAT3 pos;
+    XMFLOAT4 color;
+};
+
+struct VertexUV
+{
+    XMFLOAT3 pos;
+    //texture;
+};
+
+struct VertexNorm
+{
+    XMFLOAT3 pos;
+    XMFLOAT3 norm;
+};
+
+union Vertex
+{
+    VertexColor vertex1;
+    VertexUV vertex2;
+    VertexNorm vertex3;
+};
+
+struct VertexBuffer
+{
+    std::vector<Vertex> vertexList;
+};
+
+struct IndiceBuffer
+{
+    std::vector<int> IndiceList;
+};
