1d66309990d0c732b8ea1db5469cb917704e8961
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2024-02-27 19:20:33 +0100

    Grosse avancée sur l'initialisation de l'App

--- MoteurDirectX/Renderer.cpp
+++ MoteurDirectX/Renderer.cpp
@@ -5,8 +5,116 @@ Renderer::Renderer()
 
 }
 
+<<<<<<< Updated upstream
 //int main()
 //{
 //	std::cout << "JE FUIS !!!" << std::endl;
 //	return 0;
 //}
+=======
+Renderer* Renderer::mApp = nullptr;
+Renderer* Renderer::GetApp()
+{
+	return mApp;
+}
+
+Renderer::Renderer(HINSTANCE hInstance)
+	: mhAppInst(hInstance)
+{
+	// Only one Renderer can be constructed.
+	assert(mApp == nullptr);
+	mApp = this;
+}
+
+Renderer::~Renderer()
+{
+	if (md3dDevice != nullptr)
+		FlushCommandQueue();
+}
+
+HINSTANCE Renderer::AppInst()const
+{
+	return mhAppInst;
+}
+
+HWND Renderer::MainWnd()const
+{
+	return mhMainWnd;
+}
+
+float Renderer::AspectRatio()const
+{
+	return static_cast<float>(mClientWidth) / mClientHeight;
+}
+
+bool Renderer::Get4xMsaaState()const
+{
+	return m4xMsaaState;
+}
+
+void Renderer::Set4xMsaaState(bool value)
+{
+	if (m4xMsaaState != value)
+	{
+		m4xMsaaState = value;
+
+		// Recreate the swapchain and buffers with new multisample settings.
+		CreateSwapChain();
+		OnResize();
+	}
+}
+
+int Renderer::Run()
+{
+	MSG msg = { 0 };
+
+	//mTimer.Reset();
+
+	while (msg.message != WM_QUIT)
+	{
+		// If there are Window messages then process them.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		// Otherwise, do animation/game stuff.
+		else
+		{
+			//mTimer.Tick();
+
+			if (!mAppPaused)
+			{
+				//CalculateFrameStats();
+				//Update(mTimer);
+				//Draw(mTimer);
+			}
+			else
+			{
+				Sleep(100);
+			}
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+bool Renderer::Initialize()
+{
+	if (!InitMainWindow())
+		return false;
+
+	if (!InitDirect3D())
+		return false;
+
+	// Do the initial resize code.
+	OnResize();
+
+	return true;
+}
+
+bool Renderer::InitMainWindow()
+{
+	return false;
+}
+>>>>>>> Stashed changes
--- MoteurDirectX/Renderer.h
+++ MoteurDirectX/Renderer.h
@@ -1,11 +1,77 @@
 #pragma once
+<<<<<<< Updated upstream
 #include "IncludeLib.h"
 using Microsoft::WRL::ComPtr; 
+=======
 
-class Renderer
+#include "D3DApp.h"
+
+// Environnement de débug de la Window
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+#include <DirectXMath.h>
+#include "D3DUtils.h"
+
+// Liens nécessaires de la librairie d3d12
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+>>>>>>> Stashed changes
+
+class Renderer : D3DApp
 {
 public:
 	Renderer();
 
 };
 
+<<<<<<< Updated upstream
+=======
+	HINSTANCE AppInst()const;
+	HWND      MainWnd()const;
+	float     AspectRatio()const;
+
+	bool Get4xMsaaState()const;
+	void Set4xMsaaState(bool value);
+
+	int Run();
+
+	virtual bool Initialize();
+	virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+protected: // Fonctions protégées
+
+	bool InitMainWindow();
+
+public: // Variables protégées
+
+	static Renderer* mApp;
+
+	HINSTANCE mhAppInst = nullptr; // application instance handle
+	HWND      mhMainWnd = nullptr; // main window handle
+	bool      mAppPaused = false;  // is the application paused?
+	bool      mMinimized = false;  // is the application minimized?
+	bool      mMaximized = false;  // is the application maximized?
+	bool      mResizing = false;   // are the resize bars being dragged?
+	bool      mFullscreenState = false;// fullscreen enabled
+
+	// Set true to use 4X MSAA (§4.1.8).  The default is false.
+	bool      m4xMsaaState = false;    // 4X MSAA enabled
+	UINT      m4xMsaaQuality = 0;      // quality level of 4X MSAA
+
+	// 
+	D3D12_VIEWPORT mScreenViewport;
+	D3D12_RECT mScissorRect;
+
+	// Dimension window
+	int mClientWidth = 800;
+	int mClientHeight = 600;
+};
+>>>>>>> Stashed changes
