ff266842df67346282a0a3bd94106d6b8cad4e38
Mathieu <117281622+Kawazam@users.noreply.github.com>
2024-02-29 16:30:20 +0100

    Components' folder

---
+++ MoteurDirectX/Components/Move.cpp
@@ -0,0 +1,18 @@
+#include "./Move.h"
+#include "../Component.h"
+
+
+// Constru & Destru
+Move::Move(float ARGfVelocity = 5, XMFLOAT3 ARGv3VectorDirector)
+{
+
+}
+Move::~Move()
+{
+
+}
+
+void Move::UpdateTransform()
+{
+
+}
