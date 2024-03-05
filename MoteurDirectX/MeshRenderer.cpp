9e8360b715178d9ce70e1e118c1953740c69386b
Léo <lclipet@gaming.tech>
2024-03-05 10:00:28 +0100

    Init Shader fonctionnel

--- MoteurDirectX/MeshRenderer.cpp
+++ MoteurDirectX/MeshRenderer.cpp
@@ -12,8 +12,8 @@ MeshRenderer::~MeshRenderer()
 void MeshRenderer::GetMeshesAndShaders(const std::string& name)
 {
 	// Recupere les meshes et les shaders 
-	Mesh* mesh;
-	Shader* shader;
+	Mesh* mesh = nullptr;
+	Shader* shader = nullptr;
 
 	//mesh->GetMesh(name);
 	//shader->GetShader();
