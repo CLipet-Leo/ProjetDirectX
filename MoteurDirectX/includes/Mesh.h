bb2bd4afe98d77e0fca27268c2819cfb1f2c28ce
Riuck01 <tforest@gaming.tech>
2024-03-08 15:08:22 +0100

    remise en marche

--- MoteurDirectX/includes/Mesh.h
+++ MoteurDirectX/includes/Mesh.h
@@ -16,10 +16,9 @@ public:
 	MeshGeometry* GetMeshGeometry();
 
 	void BuildCubeGeometry(Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList);
+	void BuildPyramidGeometry(Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList);
 	virtual void BuildGeometry(Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList, 
 		const std::array<VertexColor, 8>& vertices, const std::array<std::uint16_t, 36>& indices);
-	void BuildCubeGeometry(Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList);
-	void BuildPyramidGeometry(Microsoft::WRL::ComPtr<ID3D12Device> d3dDevice, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CommandList);
 
 
 	MeshGeometry* mBoxGeo = nullptr;
